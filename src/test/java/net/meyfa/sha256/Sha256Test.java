63143fc7e655f236a6bfaf675be4c7bddcf29d53
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2020-08-09 13:51:53 +0200

    Change code formatting to be more standard

--- src/test/java/net/meyfa/sha256/Sha256Test.java
+++ src/test/java/net/meyfa/sha256/Sha256Test.java
@@ -1,23 +1,19 @@
 package net.meyfa.sha256;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 import javax.xml.bind.DatatypeConverter;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
-
-public class Sha256Test
-{
+public class Sha256Test {
     // HASHING - hash(byte[])
 
     @Test
-    public void testHashEmpty()
-    {
+    public void testHashEmpty() {
         byte[] b = {};
         byte[] expected = DatatypeConverter.parseHexBinary(
                 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
@@ -26,8 +22,7 @@ public class Sha256Test
     }
 
     @Test
-    public void testHashRegular()
-    {
+    public void testHashRegular() {
         byte[] b = "Hello world!".getBytes(StandardCharsets.US_ASCII);
         byte[] expected = DatatypeConverter.parseHexBinary(
                 "c0535e4be2b79ffd93291305436bf889314e4a3faec05ecffcbb7df31ad9e51a");
@@ -36,8 +31,7 @@ public class Sha256Test
     }
 
     @Test
-    public void testHashLong()
-    {
+    public void testHashLong() {
         byte[] b = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
                 + "Proin pulvinar turpis purus, sit amet dapibus magna commodo "
                 + "quis metus.").getBytes(StandardCharsets.US_ASCII);
@@ -48,8 +42,7 @@ public class Sha256Test
     }
 
     @Test
-    public void testHashRawBytes()
-    {
+    public void testHashRawBytes() {
         byte[] b = new byte[256];
         for (int i = 0; i < b.length; ++i) {
             b[i] = (byte) i;
@@ -62,12 +55,9 @@ public class Sha256Test
     }
 
     @Test
-    public void testHash55()
-    {
+    public void testHash55() {
         byte[] b = new byte[55];
-        for (int i = 0; i < b.length; ++i) {
-            b[i] = (byte) 'a';
-        }
+        Arrays.fill(b, (byte) 'a');
 
         byte[] expected = DatatypeConverter.parseHexBinary(
                 "9f4390f8d30c2dd92ec9f095b65e2b9ae9b0a925a5258e241c9f1e910f734318");
@@ -78,19 +68,17 @@ public class Sha256Test
     // PADDING
 
     @Test
-    public void testPaddedLengthDivisibleBy512()
-    {
+    public void testPaddedLengthDivisibleBy512() {
         byte[] b = { 0, 1, 2, 3, 0 };
 
         byte[] padded = Sha256.pad(b);
         int paddedLengthBits = padded.length * 8;
 
-        assertTrue(paddedLengthBits % 512 == 0);
+        assertEquals(0, paddedLengthBits % 512);
     }
 
     @Test
-    public void testPaddedMessageHas1Bit()
-    {
+    public void testPaddedMessageHas1Bit() {
         byte[] b = new byte[64];
 
         byte[] padded = Sha256.pad(b);
@@ -99,8 +87,7 @@ public class Sha256Test
     }
 
     @Test
-    public void testPaddingAllZero()
-    {
+    public void testPaddingAllZero() {
         byte[] b = { 1, 1, 1, 1, 1, 1, 1, };
 
         byte[] padded = Sha256.pad(b);
@@ -113,8 +100,7 @@ public class Sha256Test
     // INT ARRAY CONSTRUCTION
 
     @Test
-    public void testToIntArrayEmpty()
-    {
+    public void testToIntArrayEmpty() {
         byte[] input = {};
         int[] expected = {};
 
@@ -122,8 +108,7 @@ public class Sha256Test
     }
 
     @Test
-    public void testToIntArrayMultiple()
-    {
+    public void testToIntArrayMultiple() {
         byte[] input = { 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 3 };
         int[] expected = { 1, 2, 3 };
 
@@ -131,16 +116,14 @@ public class Sha256Test
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testToIntArrayThrowsForIllegalLength()
-    {
+    public void testToIntArrayThrowsForIllegalLength() {
         Sha256.toIntArray(new byte[] { 0, 0, 0, 1, 0 });
     }
 
     // BYTE ARRAY CONSTRUCTION
 
     @Test
-    public void testToByteArrayEmpty()
-    {
+    public void testToByteArrayEmpty() {
         int[] input = {};
         byte[] expected = {};
 
@@ -148,8 +131,7 @@ public class Sha256Test
     }
 
     @Test
-    public void testToByteArrayMultiple()
-    {
+    public void testToByteArrayMultiple() {
         int[] input = { 1, 2, 3 };
         byte[] expected = { 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 3 };
 
